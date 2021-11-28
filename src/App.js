7775e4935f2a61ebe14738bdf15b4f1edd7175d1
Raja0514 <rajamr1987@gmail.com>
2021-11-28 21:14:29 +0400

    commit1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import Homepage from "./components/homepage/homepage";
+import Login from "./components/login/login"
+import Register from"./components/register/register"
+import "./App.css"
+import { useState } from "react";
 function App() {
+
+  const[user,setLoginUser]=useState({})
+
   return (
     <div className="App">
-      <h1>welcome to oman</h1>
+
+      <Router>
+        <Switch>
+          <Route  exact path="/">
+
+           {
+
+             user && user. _id
+             ?
+             <Homepage setLoginUser={setLoginUser}/>
+             :
+             <Login setLoginUser={setLoginUser}/>
+           }
+           </Route>
+          <Route   path="/login"><Login setLoginUser={setLoginUser}/></Route>
+          <Route  path="/register"><Register/></Route>
+        </Switch>
+      </Router> 
+
     </div>
   );
 }
-
 export default App;
